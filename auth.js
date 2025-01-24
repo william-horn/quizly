2828a6a98a673c1f676770fb6b88760d2bc3de8e
William Horn <williamjosephhorn@gmail.com>
2025-01-24 05:18:25 -0500

    TEST removing Google provider parameters

--- auth.js
+++ auth.js
@@ -1,13 +1,12 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
-console.log("running auth");
-
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
-    }),
+    // Google({
+    //   clientId: process.env.AUTH_GOOGLE_ID,
+    //   clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    // }),
+    Google,
   ],
 });
