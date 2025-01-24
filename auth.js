1e18217f789a6b5422b7cb477ae6306b6cc875e9
William Horn <williamjosephhorn@gmail.com>
2025-01-24 05:03:50 -0500

    trivial changes

--- auth.js
+++ auth.js
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
+console.log("running auth");
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Google({
